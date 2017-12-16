f814d0c2824eef302700548ac66eb968860eca03
VictorZelenin <zelenin.victor95@gmail.com>
2017-12-16 16:46:23 +0200

    Removed redundant class.

--- src/main/java/ua/kpi/cad/iadms/rgr/GrapViewerTest.java
+++ src/main/java/ua/kpi/cad/iadms/rgr/GrapViewerTest.java
@@ -11,6 +11,7 @@ import java.io.FileWriter;
 import java.io.PrintWriter;
 
 public class GrapViewerTest {
+
     public static void main(String[] args) throws Exception {
         Classifier classifier = buildClassifier();
 
