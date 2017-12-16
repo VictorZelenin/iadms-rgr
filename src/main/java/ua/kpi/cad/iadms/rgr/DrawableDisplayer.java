2fafbe49950f2ae19e4c4f17c37742464fc71f2d
Oleksandr Buhaiov <olekandr.buhaiov@gmail.com>
2017-12-16 15:35:25 +0200

    adding component for tree drawing + demo of this component in Main class

---
+++ src/main/java/ua/kpi/cad/iadms/rgr/DrawableDisplayer.java
@@ -0,0 +1,55 @@
+package ua.kpi.cad.iadms.rgr;
+
+import weka.core.Drawable;
+import weka.gui.treevisualizer.PlaceNode2;
+import weka.gui.treevisualizer.TreeVisualizer;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class DrawableDisplayer {
+
+    private final Drawable drawable;
+    private JFrame frame;
+
+    public DrawableDisplayer(Drawable drawable) {
+        this.drawable = drawable;
+    }
+
+    public void display() {
+        initializeFrame();
+        Component component = getComponent(drawable);
+        display(component);
+    }
+
+    private Component getComponent(Drawable drawable) {
+        try {
+            String graph = drawable.graph();
+            return new TreeVisualizer(null, graph, new PlaceNode2());
+        } catch (Exception e) {
+            return new TextArea("Error occured while building the tree");
+        }
+    }
+
+    private void initializeFrame() {
+        frame = new JFrame("Decision Tree");
+        frame.setSize(800, 600);
+        frame.getContentPane().setLayout(new BorderLayout());
+        frame.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                frame.dispose();
+            }
+        });
+    }
+
+    private void display(Component component) {
+        frame.getContentPane().add(component, BorderLayout.CENTER);
+        frame.setVisible(true);
+        if (component instanceof TreeVisualizer) {
+            ((TreeVisualizer) component).fitToScreen();
+        }
+    }
+
+}
